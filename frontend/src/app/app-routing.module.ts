0174f6854de1a7b5bd85d8dd03f2800e260e4715
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2024-04-11 23:15:09 -0400

    edited home and project pages. Deleted About and contact pages.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ProjectsPageComponent } from './components/pages/projects-page/projects-page.component';
-import { ContactPageComponent } from './components/pages/contact-page/contact-page.component';
-import { AboutMePageComponent } from './components/pages/about-me-page/about-me-page.component';
+
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'aboutMe', component:AboutMePageComponent},
-  {path:'contact', component:ContactPageComponent},
   {path: 'search/:searchTerm',component:ProjectsPageComponent},
   {path: 'tag/:tag', component: ProjectsPageComponent},
   {path: 'projects-page', component:ProjectsPageComponent}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ProjectsPageComponent } from './components/pages/projects-page/projects-page.component';
-import { ContactPageComponent } from './components/pages/contact-page/contact-page.component';
-import { AboutMePageComponent } from './components/pages/about-me-page/about-me-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ProjectsPageComponent,
-    ContactPageComponent,
-    AboutMePageComponent
+    ProjectsPageComponent
   ],
   imports: [
     BrowserModule,
